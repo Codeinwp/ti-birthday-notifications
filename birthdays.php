38e519ab9e0ac6790b10cc94d654978681f71364
Rodica-Elena Andronache <rodica.andronache@vertistudio.com>
2017-04-11 21:44:10 +0300

    Create birthdays.php

---
+++ birthdays.php
@@ -0,0 +1,3 @@
+https://hooks.slack.com/services/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 18.04.1980
+https://hooks.slack.com/services/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 29.01.1991
+https://hooks.slack.com/services/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 04.11.1991
